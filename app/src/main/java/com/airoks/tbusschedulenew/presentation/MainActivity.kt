bfad237bdd32159671af4cee8e601a8e9420a732
Airoks <daniyrsuyuchba@gmail.com>
2020-01-03 21:32:30 +0500

    Initial commit

---
+++ app/src/main/java/com/airoks/tbusschedulenew/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.airoks.tbusschedulenew.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airoks.tbusschedulenew.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
